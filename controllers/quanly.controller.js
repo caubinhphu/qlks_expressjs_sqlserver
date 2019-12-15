99e6cc3b1106d685bd3305f11db9a3b0c740c46b
Hai tran do <10a3nguyenanhhai@gmail.com>
2019-12-15 15:17:29 -0800

    add cap nhat gia cho quan ly

--- controllers/quanly.controller.js
+++ controllers/quanly.controller.js
@@ -7,11 +7,15 @@ module.exports.thongKeLoaiPhong = async (req, res, next) => {
     await pool.connect();
     var request = new sql.Request(pool);
     var result = await request.execute("SP_THONGKE_LOAIPHONG");
+    var dsInfo = await request.execute('SP_DANHSACH_CAPNHAT_GIA');
     res.render('quanly/loaiphong', {
       title: 'Vitamin Sea: Thống kê loại phòng',
       active: 'tab1',
       dsThongKe: result.recordset,
-      user: req.signedCookies.user
+      user: req.signedCookies.user,
+      dsLoaiPhong: dsInfo.recordsets[0],
+      dsPhong: dsInfo.recordsets[1],
+      dsDichVu: dsInfo.recordsets[2],
     })
   } catch (err) {
     next(err);
@@ -25,11 +29,15 @@ module.exports.thongKePhong = async (req, res, next) => {
     await pool.connect();
     var request = new sql.Request(pool);
     var result = await request.execute("SP_THONGKE_PHONG");
+    var dsInfo = await request.execute('SP_DANHSACH_CAPNHAT_GIA');
     res.render('quanly/phong', {
       title: 'Vitamin Sea: Thống kê phòng',
       active: 'tab2',
       dsThongKe: result.recordset,
-      user: req.signedCookies.user
+      user: req.signedCookies.user,
+      dsLoaiPhong: dsInfo.recordsets[0],
+      dsPhong: dsInfo.recordsets[1],
+      dsDichVu: dsInfo.recordsets[2],
     })
   } catch (err) {
     next(err);
@@ -43,11 +51,15 @@ module.exports.thongKeDichVu = async (req, res, next) => {
     await pool.connect();
     var request = new sql.Request(pool);
     var result = await request.execute("SP_THONGKE_DICHVU");
+    var dsInfo = await request.execute('SP_DANHSACH_CAPNHAT_GIA');
     res.render('quanly/dichvu', {
       title: 'Vitamin Sea: Thống kê dịch vụ',
       active: 'tab3',
       dsThongKe: result.recordset,
-      user: req.signedCookies.user
+      user: req.signedCookies.user,
+      dsLoaiPhong: dsInfo.recordsets[0],
+      dsPhong: dsInfo.recordsets[1],
+      dsDichVu: dsInfo.recordsets[2],
     })
   } catch (err) {
     next(err);
@@ -57,13 +69,46 @@ module.exports.thongKeDichVu = async (req, res, next) => {
 };
 
 module.exports.postGiaLoaiPhong = async (req, res, next) => {
-
+  try {
+    await pool.connect();
+    var request = new sql.Request(pool);
+    request.input('MALP', req.body.MALP);
+    request.input('GIA', parseInt(req.body.GIA));
+    await request.execute("SP_CAPNHAT_GIA_LOAIPHONG");
+    res.redirect('/quanly/thongke-loaiphong');
+  } catch (err) {
+    next(err);
+  } finally {
+    await pool.close();
+  }
 };
 
 module.exports.postGiaPhong = async (req, res, next) => {
-
+  try {
+    await pool.connect();
+    var request = new sql.Request(pool);
+    request.input('MAP', req.body.MAP);
+    request.input('GIA', parseInt(req.body.GIA));
+    await request.execute("SP_CAPNHAT_GIA_PHONG");
+    res.redirect('/quanly/thongke-phong');
+  } catch (err) {
+    next(err);
+  } finally {
+    await pool.close();
+  }
 };
 
 module.exports.postGiaDichVu = async (req, res, next) => {
-
+  try {
+    await pool.connect();
+    var request = new sql.Request(pool);
+    request.input('MADV', req.body.MADV);
+    request.input('GIA', parseInt(req.body.GIA));
+    await request.execute("SP_CAPNHAT_GIA_DICHVU");
+    res.redirect('/quanly/thongke-dichvu');
+  } catch (err) {
+    next(err);
+  } finally {
+    await pool.close();
+  }
 };
